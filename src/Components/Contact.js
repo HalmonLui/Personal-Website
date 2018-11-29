721c93a088588bd04eb1b47fc871d4f02edbc397
U-WIT\luih <luih@WIT45000398.wit.private>
2018-11-29 03:21:10 -0500

    update favicon and email

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,22 +1,33 @@
-import React, { Component } from 'react';
-import emailicon from '../images/email-icon.png';
-import phoneicon from '../images/phone-icon.png';
-import contacticon from '../images/contact-icon.png';
+import React, { Component } from "react";
+import emailicon from "../images/email-icon.png";
+import phoneicon from "../images/phone-icon.png";
+import contacticon from "../images/contact-icon.png";
 class Contact extends Component {
   render() {
-    return(
+    return (
       <div className="contacts">
-        <a className="hyperlink" target="_blank" href="mailto:luih@wit.edu">
+        <a
+          className="hyperlink"
+          target="_blank"
+          href="mailto:halmonlui@gmail.com"
+        >
           <p className="contact">
-            <img src={emailicon} width="25px" alt="email icon"/> luih@wit.edu
+            <img src={emailicon} width="25px" alt="email icon" />{" "}
+            halmonlui@gmail.com
           </p>
         </a>
         <p className="contact">
-          <img src={phoneicon} width="25px" alt="cellphone icon"/> (617) 733-0634
+          <img src={phoneicon} width="25px" alt="cellphone icon" /> (617)
+          733-0634
         </p>
-        <a className="hyperlink" target="_blank" href="https://www.linkedin.com/in/halmonlui">
+        <a
+          className="hyperlink"
+          target="_blank"
+          href="https://www.linkedin.com/in/halmonlui"
+        >
           <p className="contact">
-            <img src={contacticon} width="25px" alt="linkedin icon"/> www.linkedin.com/in/halmonlui
+            <img src={contacticon} width="25px" alt="linkedin icon" />{" "}
+            www.linkedin.com/in/halmonlui
           </p>
         </a>
       </div>
